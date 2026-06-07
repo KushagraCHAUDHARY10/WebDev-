6fbffe626cdbb26641d7e7521720c31d1757c9bc
KushagraCHAUDHARY <ch.kushagra1@gmail.com>
2026-06-07 23:42:45 +0530

    done with 7th step

--- zip_files/Simon Game Challenge Starting Files/game.js
+++ zip_files/Simon Game Challenge Starting Files/game.js
@@ -1,19 +1,43 @@
 const gamePattern = [];
 var userClickedPattern = [];
 const buttonColor = ["red", "blue", "green", "yellow"];
+var started = false;
+var level = 0;
+
+$(document).on("keydown", function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$("button").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
 
 function nextSequence(){
+    level++;
+    $("#level-title").text("Level " + level);
     var next = Math.floor(Math.random*4);
     var randomChosenColour = buttonColor[next];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    var audio = new Audio("./sounds" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+    
+}
+
+function playSound(name){
+    var audio = new Audio("./sounds" + name + ".mp3");
     audio.play;
 }
 
-function userClick(){
-    $("button").click(function(){
-        var userChosenColour = $(this).attr("id");
-        userClickedPattern.push(userChosenColour);
-    });
+function animatePress(currentColor){
+    $("#" + currentColor).addClass(".pressed");
+    setTimeout(function(){
+        $("#" + currentColor).removeClass(".pressed");
+    }, 1000);
 }
