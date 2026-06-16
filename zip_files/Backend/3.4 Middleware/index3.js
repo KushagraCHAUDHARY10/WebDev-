97195251385fceb22310664a8881c34e25882bea
KushagraCHAUDHARY <ch.kushagra1@gmail.com>
2026-06-16 22:24:53 +0530

    middleware

--- zip_files/Backend/3.4 Middleware/index3.js
+++ zip_files/Backend/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Req Method: " + req.method);
+  console.log("Req URL: " + req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
